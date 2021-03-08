3fc7df0770563072bf8dc6b8dc32e976ea4643e4
nicholas-oduor <oduor5742@gmail.com>
2021-03-08 08:44:16 +0300

    added decorator auth.route and a Login function

---
+++ app/auth/views.py
@@ -0,0 +1,24 @@
+from . import auth
+from flask import render_template,redirect,url_for, flash,request
+from flask_login import login_user,logout_user,login_required
+from ..models import User,Writer
+from ..request import get_quote
+from .forms import LoginForm,RegistrationForm,WriterRegistrationForm,WriterLoginForm
+from .. import db
+from ..email import mail_message
+
+
+@auth.route('/login/user',methods=['GET','POST'])
+def login():
+    quote = get_quote()
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Nicholas blog website"
+    return render_template('auth/login.html',login_form = login_form,title=title, quote=quote)
