e8ee8e6cac3f30e5aa62d503a2bcbbcf650f33aa
nicholas-oduor <oduor5742@gmail.com>
2021-03-08 15:24:32 +0300

    added function that returns comments & updated opinion instance

--- app/main/views.py
+++ app/main/views.py
@@ -53,3 +53,66 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form, quote=quote)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    quote = get_quote()
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
+@main.route('/writer/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_writer_pic(uname):
+    quote = get_quote()
+    writer = Writer.query.filter_by(writer_name = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        Writer.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
+@main.route('/opinion/new_opinion', methods = ['GET','POST'])
+@login_required
+def new_opinion():
+    quote = get_quote()
+
+    form = OpinionForm()
+
+    if form.validate_on_submit():
+        opinion= form.description.data
+        title=form.opinion_title.data
+
+        # Updated opinion instance
+        new_opinion = Opinion(opinion_title=title,description= opinion,user_id=current_user.id)
+
+        title='New opinion'
+
+        new_opinion.save_opinion()
+
+        return redirect(url_for('main.new_opinion'))
+
+    return render_template('opinion.html',form= form, quote=quote)
+
+@main.route('/opinion/all', methods=['GET', 'POST'])
+@login_required
+def all():
+    opinions = Opinion.query.all()
+    quote = get_quote()
+    return render_template('opinions.html', opinions=opinions, quote=quote)
+@main.route('/comments/<id>')
+@login_required
+def comment(id):
+    '''
+    function to return the comments
+    '''
+    quote = get_quote()
+    comm =Comment.get_comments(id)
+    title = 'comments'
+    return render_template('comments.html',comment = comm,title = title,quote=quote)
