6ed164dfc166fc07b215e097d715615740900394
nicholas-oduor <oduor5742@gmail.com>
2021-03-08 11:31:06 +0300

    modified config.py file

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+class Config:
+    QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+}
