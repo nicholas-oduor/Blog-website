c824070f942604568d1e28fff5597542b90f47db
nicholas-oduor <oduor5742@gmail.com>
2021-03-08 08:50:25 +0300

    added a register form function

--- app/auth/views.py
+++ app/auth/views.py
@@ -36,3 +36,18 @@ def writer_login():
 
     title = "Nicholas blog website"
     return render_template('auth/writer_login.html',login_form = login_form,title=title, quote=quote)
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    quote = get_quote()
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to Nicholas Oduor blog website","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form, quote=quote)
